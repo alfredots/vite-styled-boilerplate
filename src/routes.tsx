cabc4ababaac184b5174f8116afc709a157a3b95
alfredo <alfredtito97@gmail.com>
2022-06-09 10:27:42 -0300

    feat: added styled-components and react-router-dom

---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter, Routes as ReactRouter, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+
+export const Routes = () => {
+  return (
+    <BrowserRouter>
+      <ReactRouter>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </ReactRouter>
+    </BrowserRouter>
+  )
+}
